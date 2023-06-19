960eb80d93ae38adbfcb03bdd6a775ed68260528
GloriaCoralCerecedo <gloria_marceline@hotmail.com>
2023-06-19 12:39:30 -0500

    Actualización de la migraciones

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             //El usuario se registra y si vertifica el correo se verifica la fecha en que se verifico el correo 
             //nullable -> A la hora de realizar el registro este campo quedara vacio  y si no le pasamos el campo nullable nos daria un error.
             $table->string('password'); // Contraseñas
-            $table->string('avatar'); //Avatar del usuario
             $table->rememberToken(); //Crea una columna de tipo varchar de tamaño 100, se almacena un token, cada vez que el usuario inicie sesión
             //Y marque la opción de "Mantener la sesión iniciada."
             $table->timestamps(); //Crea dos columnas de su mismo tipo. Cada vez que introduzcamos un nuevo registro
