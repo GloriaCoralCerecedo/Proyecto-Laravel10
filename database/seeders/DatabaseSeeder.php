fdc555f207d2cf2d1989ef6718c8569f68eda150
GloriaCoralCerecedo <gloria_marceline@hotmail.com>
2023-06-22 17:09:38 -0500

    factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use Illuminate\Database\Seeder;
+use App\Models\Curso;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,12 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call(CursoSeeder::class);
+        Curso::factory(50)->create();
     }
 }
